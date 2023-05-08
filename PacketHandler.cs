b4b30d47b160042f827e00b85a2e07b274731408
zmflqhaos <zmflqhaos123@gmail.com>
2023-05-08 09:15:08 +0900

    cs

---
+++ PacketHandler.cs
@@ -0,0 +1,45 @@
+﻿using DummyClient;
+using ServerCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+class PacketHandler
+{
+	public static void S_BroadcastEnterGameHandler(PacketSession session, IPacket packet)
+	{
+		//게임에 처음 접속하면 실행한다.
+		S_BroadcastEnterGame pkt = packet as S_BroadcastEnterGame;
+		ServerSession serverSession = session as ServerSession;
+
+		PlayerManager.Instance.EnterGame(pkt);
+	}
+
+	public static void S_BroadcastLeaveGameHandler(PacketSession session, IPacket packet)
+	{
+		//게임을 종료할 때 실행한다.
+		S_BroadcastLeaveGame pkt = packet as S_BroadcastLeaveGame;
+		ServerSession serverSession = session as ServerSession;
+
+		PlayerManager.Instance.LeaveGame(pkt);
+	}
+
+	public static void S_PlayerListHandler(PacketSession session, IPacket packet)
+	{
+		//새로운 플레이어를 생성한다.
+		S_PlayerList pkt = packet as S_PlayerList;
+		ServerSession serverSession = session as ServerSession;
+
+		PlayerManager.Instance.Add(pkt);
+	}
+
+	public static void S_BroadcastMoveHandler(PacketSession session, IPacket packet)
+	{
+		//플레이어를 이동시킨다.
+		S_BroadcastMove pkt = packet as S_BroadcastMove;
+		ServerSession serverSession = session as ServerSession;
+
+		PlayerManager.Instance.Move(pkt);
+	}
+}
---
+++ PlayerManager.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager
+{
+    MyPlayer _myPlayer;
+    Dictionary<int, Player> _players = new Dictionary<int, Player>();
+
+    public static PlayerManager Instance { get; } = new PlayerManager();
+
+    public void Add(S_PlayerList packet)
+    {
+        // S_PlayerList의 플레이어 숫자만큼 플레이어를 생성한다.
+        Object obj = Resources.Load("Player");
+
+        foreach (S_PlayerList.Player p in packet.players)
+        {
+            GameObject go = Object.Instantiate(obj) as GameObject;
+
+            //자신의 플레이어라면 _myPlayer에 할당한다.
+            if (p.isSelf)
+            {
+                MyPlayer myPlayer = go.AddComponent<MyPlayer>();
+                myPlayer.PlayerId = p.playerId;
+                myPlayer.transform.position = new Vector3(p.posX, p.posY, p.posZ);
+                _myPlayer = myPlayer;
+            }
+            //아니라면 플레이어리스트에 Add한다
+            else
+            {
+                Player player = go.AddComponent<Player>();
+                player.PlayerId = p.playerId;
+                player.transform.position = new Vector3(p.posX, p.posY, p.posZ);
+                _players.Add(p.playerId, player);
+            }
+        }
+    }
+
+    public void Move(S_BroadcastMove packet)
+    {
+        //패킷의 id와 자신의 id가 같으면 자신의 플레이어를 받은 위치로 이동시킨다.
+        if (_myPlayer.PlayerId == packet.playerId)
+        {
+            _myPlayer.transform.position = new Vector3(packet.posX, packet.posY, packet.posZ);
+        }
+        //아니라면 패킷의 id를 가진 플레이어를 찾아 위치를 이동시킨다.
+        else
+        {
+            Player player = null;
+            if (_players.TryGetValue(packet.playerId, out player))
+            {
+                player.transform.position = new Vector3(packet.posX, packet.posY, packet.posZ);
+            }
+        }
+    }
+
+    public void EnterGame(S_BroadcastEnterGame packet)
+    {
+        if (packet.playerId == _myPlayer.PlayerId)
+            return;
+
+        Object obj = Resources.Load("Player");
+        GameObject go = Object.Instantiate(obj) as GameObject;
+
+		Player player = go.AddComponent<Player>();
+		player.transform.position = new Vector3(packet.posX, packet.posY, packet.posZ);
+		_players.Add(packet.playerId, player);
+	}
+
+    public void LeaveGame(S_BroadcastLeaveGame packet)
+    {
+        if (_myPlayer.PlayerId == packet.playerId)
+        {
+            GameObject.Destroy(_myPlayer.gameObject);
+            _myPlayer = null;
+        }
+        else
+        {
+            Player player = null;
+            if (_players.TryGetValue(packet.playerId, out player))
+            {
+                GameObject.Destroy(player.gameObject);
+                _players.Remove(packet.playerId);
+            }
+        }
+    }
+}
